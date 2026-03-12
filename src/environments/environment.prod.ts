a69185dd535b44547916ded0951ba04f361dbd08
Diego Ferrer <diegoferrer@Diegos-Mac-mini.local>
2026-03-12 06:54:20 -0300

    proxy-vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
     googleOAuthClientId: '159298277001-dbgqonuep9kohnr190224umlu8m19gpd.apps.googleusercontent.com',
-    apiUrl: 'https://iflow-backend.onrender.com/api',
+    //apiUrl: 'https://iflow-backend.onrender.com/api',
+    apiUrl: '/api',
 };
