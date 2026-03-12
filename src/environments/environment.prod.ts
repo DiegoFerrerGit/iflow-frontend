9a56d94cfca34dd56327777101484cc307f255db
Diego Ferrer <diegoferrer@Diegos-Mac-mini.local>
2026-03-12 18:33:37 -0300

    configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     googleOAuthClientId: '159298277001-dbgqonuep9kohnr190224umlu8m19gpd.apps.googleusercontent.com',
     //apiUrl: 'https://iflow-backend.onrender.com/api',
-    apiUrl: '/api',
+    apiUrl: "https://api.thotifa.com/api",
 };
